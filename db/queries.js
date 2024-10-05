f027fcb6892887406a9c561030b08da35229d46d
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-10-05 17:31:45 +0200

    Add 2queries, add 1 row to insertNewUser querie

--- db/queries.js
+++ db/queries.js
@@ -2,14 +2,20 @@ const pool = require("./pool");
 
 async function insertNewUser(user, hashedPassword) {
   await pool.query(
-    "INSERT INTO users (username, first_name, last_name, password, membership_status) VALUES ($1, $2, $3, $4, $5)",
-    [user.userName, user.firstName, user.lastName, hashedPassword, false]
+    "INSERT INTO users (username, first_name, last_name, password, membership_status, admin_status) VALUES ($1, $2, $3, $4, $5, $6)",
+    [user.userName, user.firstName, user.lastName, hashedPassword, false, false]
   );
 }
 
 async function updateUserMembership(username) {
   await pool.query(
     `UPDATE users SET membership_status = true WHERE username = '${username}'`
+  );
+}
+
+async function updateUserAdmin(username) {
+  await pool.query(
+    `UPDATE users SET admin_status = true WHERE username = '${username}'`
   );
 }
 
@@ -33,9 +39,17 @@ async function getAllMessages() {
   return rows;
 }
 
+async function deleteMessage(messageTitle) {
+  await pool.query(
+    `DELETE   FROM messages WHERE message_title  = '${messageTitle}';`
+  );
+}
+
 module.exports = {
   insertNewUser,
   updateUserMembership,
+  updateUserAdmin,
   createNewMessage,
   getAllMessages,
+  deleteMessage,
 };
