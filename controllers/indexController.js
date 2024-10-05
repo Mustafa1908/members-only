f60d361a0212e697c910d4776b387e78f6856fe2
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-10-05 17:55:49 +0200

    Add errors messages for new message view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,6 @@
 const db = require("../db/queries");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 let getHomePageGet = async (req, res) => {
   let allMessages = await db.getAllMessages();
@@ -8,23 +9,40 @@ let getHomePageGet = async (req, res) => {
 
 let getNewMessagePageGet = async (req, res) => {
   if (res.locals.currentUser === undefined) {
-    console.log("You need to be connected to write a new message");
     res.redirect("/");
     return;
   }
   res.render("new_message");
 };
 
-let addNewMessagePost = asyncHandler(async (req, res, next) => {
-  let messageDate = new Date();
+let addNewMessagePost = [
+  body("message_title")
+    .trim()
+    .isLength({ min: 5, max: 100 })
+    .withMessage(`User name must be between 5 and 100 characters`),
+  body("message")
+    .trim()
+    .isLength({ min: 20, max: 1000 })
+    .withMessage(`First name must be between 20 and 1000 characters`),
+  asyncHandler(async (req, res, next) => {
+    let messageDate = new Date();
 
-  await db.createNewMessage(
-    res.locals.currentUser.username,
-    req.body,
-    messageDate
-  );
-  res.redirect("/");
-});
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors.array());
+      return res.status(400).render("new_message", {
+        errors: errors.array(),
+      });
+    }
+
+    await db.createNewMessage(
+      res.locals.currentUser.username,
+      req.body,
+      messageDate
+    );
+    res.redirect("/");
+  }),
+];
 
 let deleteMessagePost = asyncHandler(async (req, res, next) => {
   await db.deleteMessage(req.body.messageTitle);
