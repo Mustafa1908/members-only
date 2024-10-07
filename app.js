a82e8c3a8993fd46717342075a3889e25b8419bb
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-10-07 18:03:44 +0200

    Change port value

--- app.js
+++ app.js
@@ -28,5 +28,5 @@ passwordUtils.verifyUser();
 passwordUtils.serializeUser();
 passwordUtils.deserializeUser();
 
-const PORT = process.env.PORT || 5432;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
